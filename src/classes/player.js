f08d52014bdb9b8a785d823d62cd0f94a03bb7d9
Anthony Chao <anthonyschaowder@gmail.com>
2022-07-26 11:23:47 -0400

    added input song name feature

--- src/classes/player.js
+++ src/classes/player.js
@@ -1,10 +1,9 @@
-import Sheet from "./sheet";
-
 class Player {
 
     constructor(){
 
         this.containers = document.querySelectorAll('.droppable');
+        // this.instrumentSelected = document.querySelector("select").value; 
 
     }
 
@@ -20,20 +19,36 @@ class Player {
 
     playMusic() {
 
+        //add IF statement so that if instrumentSelected is trumpet, use second hash, if it's piano, use first hash
+
         const rowToNote = new Object();
-        rowToNote[0] = document.getElementById('5a');
-        rowToNote[1] = document.getElementById('4g');
-        rowToNote[2] = document.getElementById('4f');
-        rowToNote[3] = document.getElementById('4e');
-        rowToNote[4] = document.getElementById('4d');
-        rowToNote[5] = document.getElementById('4c');
-        rowToNote[6] = document.getElementById('4b');
-        rowToNote[7] = document.getElementById('4a');
-        rowToNote[8] = document.getElementById('3g');
-        rowToNote[9] = document.getElementById('3f');
-        rowToNote[10] = document.getElementById('3e');
-        rowToNote[11] = document.getElementById('3d');
-        rowToNote[12] = document.getElementById('3c');
+        // rowToNote[0] = document.getElementById('5a');
+        // rowToNote[1] = document.getElementById('4g');
+        // rowToNote[2] = document.getElementById('4f');
+        // rowToNote[3] = document.getElementById('4e');
+        // rowToNote[4] = document.getElementById('4d');
+        // rowToNote[5] = document.getElementById('4c');
+        // rowToNote[6] = document.getElementById('4b');
+        // rowToNote[7] = document.getElementById('4a');
+        // rowToNote[8] = document.getElementById('3g');
+        // rowToNote[9] = document.getElementById('3f');
+        // rowToNote[10] = document.getElementById('3e');
+        // rowToNote[11] = document.getElementById('3d');
+        // rowToNote[12] = document.getElementById('3c');
+
+        rowToNote[0] = document.getElementById('trumpet5a');
+        rowToNote[1] = document.getElementById('trumpet4g');
+        rowToNote[2] = document.getElementById('trumpet4f');
+        rowToNote[3] = document.getElementById('trumpet4e');
+        rowToNote[4] = document.getElementById('trumpet4d');
+        rowToNote[5] = document.getElementById('trumpet4c');
+        rowToNote[6] = document.getElementById('trumpet4b');
+        rowToNote[7] = document.getElementById('trumpet4a');
+        rowToNote[8] = document.getElementById('trumpet3g');
+        rowToNote[9] = document.getElementById('trumpet3f');
+        rowToNote[10] = document.getElementById('trumpet3e');
+        rowToNote[11] = document.getElementById('trumpet3d');
+        rowToNote[12] = document.getElementById('trumpet3c');
 
         const notesToBePlayed = [];
 
@@ -49,7 +64,7 @@ class Player {
                 setTimeout(function() {
                     rowToNote[notesToBePlayed[i]].play();
                     console.log(rowToNote[notesToBePlayed[i]]);
-                }, 500 * i);
+                }, 800 * i);            //500 for piano, 800 for trumpet
             })(i);
         }
     }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const sheet = new Sheet(rootEl);
   const notes = new Notes(rootEl);
   const player = new Player();
+  // const selectInstrument = document.getElementById('instrumentSelector');
+  // let instrument = "piano";
+  // selectInstrument.addEventListener('change',function () { instrument = this.value;} ,false); 
   const playButton = document.getElementById('playButton');
   playButton.addEventListener('click', player.playMusic.bind(player));
   const resetButton = document.getElementById('resetButton');
