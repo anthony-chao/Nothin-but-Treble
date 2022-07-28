78bb83d7fee7890ba0d554a10ea77be2e2da97a0
Anthony Chao <anthonyschaowder@gmail.com>
2022-07-27 22:16:44 -0400

    converted instrument selector to button instead of dropdown

--- src/index.js
+++ src/index.js
@@ -9,13 +9,27 @@ document.addEventListener("DOMContentLoaded", () => {
   const noteRoot = document.querySelector(".Notes")
   const notes = new Notes(noteRoot);
   const player = new Player();
-  const selectInstrument = document.getElementById('instrumentSelector');
   const playButton = document.getElementById('playButton');
   let instruments = {saxophone:'saxophone', clarinet:'clarinet', trumpet:'trumpet'}
   let instrument = instruments.saxophone;
-  selectInstrument.addEventListener('change',function () { 
-    instrument = instruments[this.value];
-  }, false); 
+
+  // this code was used for creating a dropdown for the instruments.
+  // const selectInstrument = document.getElementById('instrumentSelector');
+  // selectInstrument.addEventListener('change',function () { 
+  //   instrument = instruments[this.value];
+  // }, false); 
+  const selectInstrument = document.getElementById('instrumentSelector');  
+  const radioButtons = document.querySelectorAll('input[name="radiobutton"]');
+  selectInstrument.addEventListener("click", () => {
+      let selectedSize;
+      for (const radioButton of radioButtons) {
+          if (radioButton.checked) {
+              selectedSize = radioButton.value;
+              break;
+          }
+      }
+      instrument = selectedSize
+    });
 
   playButton.addEventListener('click', () => {
     player.instrument = instrument;
